3072a0c132bc5e2196f32697ff4d12aff41a2846
Srikanth Bandari <bsrikanth@intraedge.com>
2023-06-08 07:33:26 +0530

    added tilt

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
 
 const Card = ({name, email, id}) => {
     return (
         <div className="bg-light-green dib b3 pa3 ma2 grow bw2 shadow-5">
-            <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
+            <Tilt>
+                <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+                <div>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+            </Tilt>
         </div>
     )
 }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from './Card';
-import Tilt from 'react-parallax-tilt';
 
 const CardList = ({ robots }) => {
 
@@ -9,14 +8,12 @@ const CardList = ({ robots }) => {
             {
                 robots.map ( (user, i) => {
                     return (
-                        <Tilt>
                             <Card
                                 key={i}
                                 id={robots[i].id}
                                 name={robots[i].name}
                                 email={robots[i].email}
                             />
-                        </Tilt>
                     )
                 })
             }
